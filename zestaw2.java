d1f228693740f7f963860b4c208b0df973e7b5f3
NataliaSkrodzka <161207800+NataliaSkrodzka@users.noreply.github.com>
2024-10-16 14:54:56 +0200

    Create zestaw2.java

---
+++ zestaw2.java
@@ -0,0 +1,31 @@
+public class Zestaw2 {
+    public static void main(String[] args) {
+        //zad 1
+        //23.0+76 - Float
+        //41*2.0+3 - Float
+        //5-33 - Integer
+        //109%3 - Integer
+        //50/2 - Integer
+        //4|2 - Integer
+        //3^5 - Integer
+        //7&9 - Integer
+        //zad 2
+        double a = (((Math.sqrt(7))-1)/2)+((Math.pow(3,3))%2);
+        System.out.println((int)(a));
+
+        int b = 1985/(24*1);
+        System.out.println(b);
+
+        double c = ((3+(Math.sqrt(3)))/((Math.sqrt(5))/2)/3)+1;
+        System.out.println((int)c);
+
+        double d = 9%2%2;
+        System.out.println((int)d);
+
+        double e = ((8%7)+1)/Math.pow(2,1/4);
+        System.out.println((int)e);
+        //zad 3
+        
+
+    }
+}
