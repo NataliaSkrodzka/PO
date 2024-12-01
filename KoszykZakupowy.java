667c21d73fab446f3c833081e3cacedd828eec8a
NataliaSkrodzka <161207800+NataliaSkrodzka@users.noreply.github.com>
2024-12-01 15:28:51 +0100

    Create KoszykZakupowy.java

---
+++ KoszykZakupowy.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class KoszykZakupowy {
+    private HashMap<Produkt, Integer> produkty;
+
+    public KoszykZakupowy() {
+        this.produkty = new HashMap<>();
+    }
+    public void dodajProdukt(Produkt produkt,int ile) {
+        if (iloscNaMagazynie>ile)
+
+    }
+}
