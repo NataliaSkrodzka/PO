494cc66ded6699eea52eb990d1b3a15fa563389e
NataliaSkrodzka <161207800+NataliaSkrodzka@users.noreply.github.com>
2024-12-02 15:47:45 +0100

    Update KoszykZakupowy.java

--- KoszykZakupowy.java
+++ KoszykZakupowy.java
@@ -7,7 +7,24 @@ public class KoszykZakupowy {
         this.produkty = new HashMap<>();
     }
     public void dodajProdukt(Produkt produkt,int ile) {
-        if (iloscNaMagazynie>ile)
-
+        if (produkt.iloscNaMagazynie>=ile){
+            produkty.put(produkt,ile);
+            produkt.usunZMagazynu(ile);
+        }
+        else {
+            System.out.println("Niewystarczajaca ilosc produktów w magazynie");
+        }
+    }
+    public void wyswietlZawartoscKoszyka(){
+        for(Produkt produkt : produkty.keySet()) {
+            System.out.println("Produkt: " + produkt.getNazwa() + " ilość: "+ produkty.get(produkt));
+        }
+    }
+    public double obliczCalkowitaWartosc(){
+        double calkowitaWartosc = 0;
+        for(Produkt produkt : produkty.keySet()) {
+            calkowitaWartosc+=produkt.getCena()*produkty.get(produkt);
+        }
+        return calkowitaWartosc;
     }
 }
