a7d3d94bacaff77803f1d4a338b25000b182ec7c
NataliaSkrodzka <161207800+NataliaSkrodzka@users.noreply.github.com>
2024-12-06 14:01:55 +0100

    Update KoszykZakupowy.java

--- KoszykZakupowy.java
+++ KoszykZakupowy.java
@@ -8,17 +8,37 @@ public class KoszykZakupowy {
     }
     public void dodajProdukt(Produkt produkt,int ile) {
         if (produkt.iloscNaMagazynie>=ile){
-            produkty.put(produkt,ile);
+            if (this.produkty.containsKey(produkt)){
+                produkty.replace(produkt,produkty.get(produkt)+ile);
+            }
+            else {
+                produkty.put(produkt, ile);
+            }
             produkt.usunZMagazynu(ile);
         }
         else {
             System.out.println("Niewystarczajaca ilosc produktów w magazynie");
         }
     }
-    public void wyswietlZawartoscKoszyka(){
-        for(Produkt produkt : produkty.keySet()) {
-            System.out.println("Produkt: " + produkt.getNazwa() + " ilość: "+ produkty.get(produkt));
+    public void usunProdukt(Produkt produkt,int ile){
+        if(produkty.containsKey(produkt)&&produkty.get(produkt)>=ile) {
+            produkty.replace(produkt,produkty.get(produkt)-ile);
+            produkt.dodajDoMagazynu(ile);
         }
+        else {
+            System.out.println("Niewystarczajaca ilosc produktów w koszyku");
+        }
+    }
+    @Override
+    public String toString(){
+        StringBuilder wynik= new StringBuilder();
+        for (Produkt produkt : produkty.keySet()) {
+            wynik.append("Produkt: ").append(produkt.getNazwa()).append(" ilość: ").append(produkty.get(produkt)).append("\n");
+        }
+        if(produkty.isEmpty()){
+            return "koszyk jest pusty";
+        }
+        return wynik.toString();
     }
     public double obliczCalkowitaWartosc(){
         double calkowitaWartosc = 0;
