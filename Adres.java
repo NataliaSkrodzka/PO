50d47bb16845ea0686ec9e75f7bac597d3f67ba9
NataliaSkrodzka <161207800+NataliaSkrodzka@users.noreply.github.com>
2024-12-07 17:12:17 +0100

    Update Adres.java

--- Adres.java
+++ Adres.java
@@ -4,6 +4,7 @@ public class Adres {
     private int numerMieszania;
     private String miasto;
     private int kodPocztowy;
+
     public Adres(String ulica, int numerDomu, int numerMieszania, String miasto, int kodPocztowy) {
         this.ulica = ulica;
         this.numerDomu = numerDomu;
@@ -17,4 +18,63 @@ public class Adres {
         this.miasto = miasto;
         this.kodPocztowy = kodPocztowy;
     }
+    public String getUlica() {
+        return ulica;
+    }
+    public void setUlica(String ulica) {
+        this.ulica = ulica;
+        if(ulica==null || ulica.isEmpty()){
+            throw new IllegalArgumentException("Ulica nie może być pusta");
+        }
+    }
+    public int getNumerDomu() {
+        return numerDomu;
+    }
+    public void setNumerDomu(int numerDomu) {
+        this.numerDomu = numerDomu;
+        if(numerDomu==0){
+            throw new IllegalArgumentException("Numerem domu nie może być zero");
+        }
+    }
+    public int getNumerMieszania() {
+        return numerMieszania;
+    }
+    public void setNumerMieszania(int numerMieszania) {
+        this.numerMieszania = numerMieszania;
+        if(numerMieszania==0){
+            throw new IllegalArgumentException("Numerem mieszkania nie może być 0");
+        }
+    }
+    public String getMiasto() {
+        return miasto;
+    }
+    public void setMiasto(String miasto) {
+        this.miasto = miasto;
+        if(miasto==null || miasto.isEmpty()){
+            throw new IllegalArgumentException("pole miasto nie może być puste");
+        }
+    }
+    public int getKodPocztowy() {
+        return kodPocztowy;
+    }
+    public void setKodPocztowy(int kodPocztowy) {
+        this.kodPocztowy = kodPocztowy;
+        if(kodPocztowy == 0){
+            throw new IllegalArgumentException("Kod pocztowy musi mieć format XX-XXX");
+        }
+    }
+
+    public void pokaz(){
+        System.out.println("kod pocztowy: " + kodPocztowy + " miasto: " + miasto);
+        if (numerMieszania == 0){
+            System.out.println("ulica: " + ulica + " numerDomu: " + numerDomu);
+        }
+        else {
+            System.out.println("ulica: " + ulica + " numerDomu: " + numerDomu + " numerMieszania: " + numerMieszania);
+        }
+    }
+    public boolean przed(int kodPocztowy){
+        return this.kodPocztowy < kodPocztowy;
+    }
+
 }
