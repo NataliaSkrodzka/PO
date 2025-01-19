9e5d2378e5d5e09279c5424081622e3ce1c260c3
NataliaSkrodzka <161207800+NataliaSkrodzka@users.noreply.github.com>
2025-01-19 14:48:40 +0100

    Update Test.java

--- kolokwium3/Test.java
+++ kolokwium3/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
         Building building=new Building(3.5,"niebieski");
         House house=new House(2.5,"czerwony",5);
-        System.out.printf(building.toString());
+        System.out.println(building);
+        System.out.println(house);
     }
 }
